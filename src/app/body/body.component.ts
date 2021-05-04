0f005b9cdb96cea995e3867c8850460c6e707e51
gitCodeTeu <marjorie.teu@gmail.com>
2021-05-03 23:02:40 -0400

    design card parts for data output

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -9,23 +9,59 @@ import { GetApiService } from '../get-api.service';
 
 export class BodyComponent implements OnInit {
 
-  txtCity = "Toronto";
+  txtCity = "toronto";
 
   constructor(private api: GetApiService) {
 
   }
-  
+
+  //data fetched
+  wdata;
+
   btnGetWeather() {
+
+  }
+
+
+  getWeather() {
     this.api.getWeather(this.txtCity).subscribe((data) => {
       console.log(data);
-    })
+      this.wdata = data;
+    });
   }
 
   ngOnInit() {
-    
+    this.txtCity = "toronto";
+    //this.getWeather();
+    this.api.getWeather(this.txtCity).subscribe((data) => {
+      console.log(data);
+      this.wdata = data;
+    });
+   
+    //display data
+    document.getElementById("table").innerHTML = `
+    <tr>
+      <th>${this.wdata.name}</th>
+      <th>temp</th>
+      <th>temp</th>
+      <th>temp</th>
+      <th>temp</th>
+    </tr>
+    <tr>
+      <td>temp</td>
+      <td>temp</td>
+      <td>temp</td>
+      <td>temp</td>
+      <td>temp</td>
+
+    </tr>
+  `;
+
+
+
   }
 
-  btnReset() {
-    this.txtCity = "";
-  }
+  // btnReset() {
+  //   this.txtCity = "";
+  // }
 }
--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -10,13 +10,13 @@ export class GetApiService {
   constructor(private http: HttpClient) { }
 
   cityName: string = "Toronto";
+  units: string = "metric";
 
   private key: string = "898c9a34f3f25e07a8dc55a283e8464c";
-  //private _url: string = ;
-
 
   getWeather(city: string) {
     this.cityName = city;
-    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${this.key}`);
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${this.key}&units=${this.units}
+    `);
   }
 }

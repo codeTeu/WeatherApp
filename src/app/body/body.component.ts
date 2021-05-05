f058049a39dba637444010e635ff995ba7d79094
gitCodeTeu <marjorie.teu@gmail.com>
2021-05-04 22:41:57 -0400

    redesign card, responsive now

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -11,54 +11,49 @@ export class BodyComponent implements OnInit {
 
   txtCity = "toronto";
 
+  //data fetched
+  wdata; 
+
+  //current date
+  date = new Date();
+
+  //temp
+  temp;
+  getIcon; 
+  iconSrc;
+
+sunrise;
+sunset;
+
   constructor(private api: GetApiService) {
 
   }
 
-  //data fetched
-  wdata;
+
+
 
   btnGetWeather() {
+    this.getWeather();
 
-  }
+     }
 
 
   getWeather() {
     this.api.getWeather(this.txtCity).subscribe((data) => {
       console.log(data);
       this.wdata = data;
+      
+      this.sunrise = new Date(this.wdata.sys.sunrise * 1000);
+      this.sunset = new Date(this.wdata.sys.sunset * 1000);
     });
+  
+    
+  
   }
 
   ngOnInit() {
     this.txtCity = "toronto";
-    //this.getWeather();
-    this.api.getWeather(this.txtCity).subscribe((data) => {
-      console.log(data);
-      this.wdata = data;
-    });
-   
-    //display data
-    document.getElementById("table").innerHTML = `
-    <tr>
-      <th>${this.wdata.name}</th>
-      <th>temp</th>
-      <th>temp</th>
-      <th>temp</th>
-      <th>temp</th>
-    </tr>
-    <tr>
-      <td>temp</td>
-      <td>temp</td>
-      <td>temp</td>
-      <td>temp</td>
-      <td>temp</td>
-
-    </tr>
-  `;
-
-
-
+    this.getWeather();
   }
 
   // btnReset() {
